7a4be0305b3992d7f3ac8d3e51db8b483963ef85
gyugle <sonmingyu23@naver.com>
2026-04-16 17:52:47 +0900

    style: MainActivity 내 import 구문 정렬 수정
    MainActivity.kt 파일의 import 구문을 알파벳 순서에 맞게 재정렬하여 코드 스타일의 일관성을 개선하였습니다.
    
    - `androidx.compose.ui.tooling.preview.Preview` 임포트 위치 조정
    - 코드 컨벤션 준수를 위한 단순 포맷팅 수정 (비즈니스 로직 변경 없음)

--- chromaflow-sample/src/main/java/com/gyugle/chromaflow/sample/MainActivity.kt
+++ chromaflow-sample/src/main/java/com/gyugle/chromaflow/sample/MainActivity.kt
@@ -23,11 +23,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gyugle.chromaflow.ChromaFlowDirection
 import com.gyugle.chromaflow.ChromaFlowImage
